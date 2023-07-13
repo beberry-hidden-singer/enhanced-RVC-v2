7de8fde2349d8001920b390e0a9890a8b36b1609
karljeon44 <karljeon44@gmail.com>
2023-07-13 09:14:01 -0400

    dev initial commmit with support for G with BigVGAN

---
+++ write_pretraining_filelist.py
@@ -0,0 +1,76 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: karljeon44
+# Date: 7/2/23 12:03 AM
+import json
+import logging
+import os
+import sys
+from random import shuffle
+
+logger = logging.getLogger(__name__)
+
+
+def main(exp_dir):
+  print("EXP Dir:", exp_dir)
+
+  gt_wavs_dir = "%s/0_gt_wavs" % exp_dir
+  feature_dir = "%s/3_feature768" % exp_dir
+  f0_dir = "%s/2a_f0" % (exp_dir)
+  f0nsf_dir = "%s/2b-f0nsf" % (exp_dir)
+  names = (
+          set([name.split(".")[0] for name in os.listdir(gt_wavs_dir)])
+          & set([name.split(".")[0] for name in os.listdir(feature_dir)])
+          & set([name.split(".")[0] for name in os.listdir(f0_dir)])
+          & set([name.split(".")[0] for name in os.listdir(f0nsf_dir)])
+  )
+
+  print("Names:", len(names))
+
+
+  speaker_mapping = dict()
+  # speaker_mapping_count =
+
+  opt = []
+  for name in names:
+    spk_id_src = name.split('_')[-1]
+    if spk_id_src not in speaker_mapping:
+      speaker_mapping[spk_id_src] = len(speaker_mapping)
+    spk_id = speaker_mapping[spk_id_src]
+    # TODO: map singer id to a unique consecutive int, starting from 0
+    opt.append(
+      "%s/%s.wav|%s/%s.npy|%s/%s.wav.npy|%s/%s.wav.npy|%s"
+      % (
+        gt_wavs_dir.replace("\\", "\\\\"),
+        name,
+        feature_dir.replace("\\", "\\\\"),
+        name,
+        f0_dir.replace("\\", "\\\\"),
+        name,
+        f0nsf_dir.replace("\\", "\\\\"),
+        name,
+        spk_id,
+      )
+    )
+
+  print("Speaker Mapping:", speaker_mapping)
+  with open('./speaker_mapping.json', 'w') as f:
+    json.dump(speaker_mapping, f)
+  for spk_id in speaker_mapping.values():
+    for _ in range(2):
+      opt.append(
+        "./logs/mute/0_gt_wavs/mute%s.wav|./logs/mute/3_feature%s/mute.npy|./logs/mute/2a_f0/mute.wav.npy|./logs/mute/2b-f0nsf/mute.wav.npy|%s"
+        % ('40k', 768, spk_id)
+      )
+
+  shuffle(opt)
+  with open("%s/filelist.txt" % exp_dir, "w") as f:
+    f.write("\n".join(opt))
+  print("write filelist done")
+
+
+if __name__ == '__main__':
+  log_dir = './logs/bebe_gas'
+  if len(sys.argv) > 1:
+    log_dir = sys.argv[1]
+  main(log_dir)
